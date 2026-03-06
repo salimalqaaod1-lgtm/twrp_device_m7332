78d4e1d07927b0ed36e1639e6cdd1c6a368a2100
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-06 20:05:23 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,29 +1,8 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/mediatek/m7332
-# Inherit TWRP common parts
-$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit AOSP base
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-# Remote Control Configs - نسخ ملفات التردد 0x00BF والتعريفات
+# نسخ ملف التردد الخاص بالريموت (تأكد من وجود الملف في المستودع)
 PRODUCT_COPY_FILES += \
-  #  $(LOCAL_PATH)/prebuilt/u_irt_keymap.kl:recovery/root/system/usr/keylayout/u_irt_keymap.kl \
-    $(LOCAL_PATH)/prebuilt/ir_config.ini:recovery/root/vendor/etc/ir_config.ini
-  #  $(DEVICE_PATH)/init.recovery.m7332.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.m7332.rc
+    $(LOCAL_PATH)/ir_config.ini:recovery/root/vendor/etc/ir_config.ini
 
-# Device Characteristics
-PRODUCT_CHARACTERISTICS := tv
-
-# Device Identifiers
-PRODUCT_NAME := omni_m7332
-PRODUCT_DEVICE := m7332
-PRODUCT_BRAND := mediatek
-PRODUCT_MODEL := M7332_TV
-PRODUCT_MANUFACTURER := mediatek
+# تم تعطيل النسخ اليدوي لملف الـ RC لأن النظام سيدمجه تلقائياً من المجلد
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/init.recovery.m7332.rc:root/init.recovery.m7332.rc
