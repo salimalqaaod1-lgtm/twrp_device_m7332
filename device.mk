79ac1088ccb8139c626deec2872e0d64c46ff93c
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-06 22:22:27 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,15 +1,14 @@
 LOCAL_PATH := device/mediatek/m7332
 
-# 1. نسخ ملفات تعريف أزرار الريموت كنترول (التي رفعتها أنت)
+# نسخ ملفات تعريف الريموت
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/Generic.kl:recovery/root/system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/AVRCP.kl:recovery/root/system/usr/keylayout/AVRCP.kl \
     $(LOCAL_PATH)/ir_config.ini:recovery/root/vendor/etc/ir_config.ini
 
-# 2. نسخ ملف خريطة الأقسام (fstab) لكي يرى TWRP الذاكرة الداخلية
+# نسخ ملف الأقسام
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
 
-# 3. استدعاء مكتبات أندرويد الأساسية اللازمة للبناء
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+# تعريف المنتج - استبدل الأسطر القديمة بهذا السطر فقط
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
