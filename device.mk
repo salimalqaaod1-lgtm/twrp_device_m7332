5fba8f47d4136dd2f7f4901af17ef8a1386dd5be
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-08 23:11:01 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := device/mediatek/m7332
 # نسخ ملفات تعريف الريموت
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/Generic.kl:recovery/root/system/usr/keylayout/Generic.kl
-    $(LOCAL_PATH)/ir_config.ini:recovery/root/vendor/etc/ir_config.ini
+    $(LOCAL_PATH)/ir_Config.ini:recovery/root/vendor/etc/ir_Config.ini
 
 # نسخ ملف الأقسام
 PRODUCT_COPY_FILES += \
