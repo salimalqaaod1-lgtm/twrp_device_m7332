dfa82a6076f637e27c16fc7b2034d758c1f287d0
salimalqaaod1-lgtm <salim.alqaaod1@gmail.com>
2026-03-08 22:06:21 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,9 +2,4 @@
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_m7332-user
-add_lunch_combo omni_m7332-userdebug
-add_lunch_combo omni_m7332-eng
+# SPDX-
